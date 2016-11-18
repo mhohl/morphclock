779d051e0a1e1ceb93e1623b6573045972199a3b
Marc Hohl <marc@hohlart.de>
2016-11-18 08:48:52 +0100

    TeX-Datei zum optischen Test

--- digits.sh
+++ digits.sh
@@ -9,7 +9,7 @@ mpost digit.mpost
 for (( i = from; i <= to; i++ )); do
    picfile="000$i"
    picfile="pic${picfile:(-3)}.png"
-   test -e digit.$i && convert digit.$i -background white -alpha remove $picfile && echo $picfile
+   test -e digit-$i.mps && convert digit-$i.mps -background white -alpha remove $picfile && echo $picfile
 done
 
 # animate pic*.png &
