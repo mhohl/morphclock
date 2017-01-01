bd0359642d6004b1163f04aec22f616e8f9f1497
Marc Hohl <marc@hohlart.de>
2017-01-01 17:28:08 +0100

    Mehrere Animationsschleifen parallel

--- digits.sh
+++ digits.sh
@@ -14,4 +14,13 @@ for (( i = from; i <= to; i++ )); do
    test -e digit-$i.mps && convert digit-$i.mps -background white -alpha remove $picfile && echo $picfile
 done
 
-# animate pic*.png &
+echo -n "Animationen abspielen? "
+read -n1 jn;
+
+if [ "$jn" != "n" ]; then
+   animate pic01*.png pic10*.png &
+   animate pic01*.png pic12*.png pic20*.png &
+   animate pic01*.png pic12*.png pic23*.png pic30*.png &
+   animate pic01*.png pic12*.png pic23*.png pic34*.png pic45*.png pic50*.png &
+   animate pic01*.png pic12*.png pic23*.png pic34*.png pic45*.png pic56*.png pic67*.png pic78*.png pic89*.png pic90*.png &
+fi
