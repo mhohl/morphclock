bd34607d16db59ac3b6a002acc40b3e10ba4221f
Marc Hohl <marc@hohlart.de>
2017-01-02 09:18:14 +0100

    Umorganisation Dateibenennung

--- digits.sh
+++ digits.sh
@@ -10,7 +10,8 @@ lualatex testdigit.tex > /dev/null
 
 for (( i = from; i <= to; i++ )); do
    picfile="0000$i"
-   picfile="pic${picfile:(-4)}.png"
+   picfile="${picfile:(-4)}"
+   picfile="md-${picfile:0:2}-${picfile:2:2}.png"
    test -e digit-$i.mps && convert digit-$i.mps $picfile && echo $picfile
 done
 
@@ -18,12 +19,20 @@ echo -n "Animationen abspielen? "
 read -n1 jn;
 
 if [ "$jn" == "j" ]; then
-   animate -dispose previous pic01*.png pic10*.png &
-   animate -dispose previous pic12*.png pic21*.png &
-   animate -dispose previous pic01*.png pic12*.png pic20*.png &
-   animate -dispose previous pic01*.png pic12*.png pic23*.png pic30*.png &
-   animate -dispose previous pic01*.png pic12*.png pic23*.png pic34*.png pic45*.png pic50*.png &
-   animate -dispose previous pic01*.png pic12*.png pic23*.png pic34*.png pic45*.png pic56*.png pic67*.png pic78*.png pic89*.png pic90*.png &
+   animate -dispose previous md-01-*.png md-10-*.png &
+   animate -dispose previous md-12-*.png md-21-*.png &
+   animate -dispose previous md-01-*.png md-12-*.png \
+                             md-20-*.png &
+   animate -dispose previous md-01-*.png md-12-*.png \
+                             md-23-*.png md-30-*.png &
+   animate -dispose previous md-01-*.png md-12-*.png \
+                             md-23-*.png md-34-*.png \
+                             md-45-*.png md-50-*.png &
+   animate -dispose previous md-01-*.png md-12-*.png \
+                             md-23-*.png md-34-*.png \
+                             md-45-*.png md-56-*.png \
+                             md-67-*.png md-78-*.png \
+                             md-89-*.png md-90-*.png &
 else
    echo
 fi
