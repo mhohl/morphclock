f15911978f1261e6df64f42415798a15c240fef0
Marc Hohl <marc@hohlart.de>
2017-01-27 11:39:55 +0100

    Bildadressen als Objekt deklariert; maxh und maxH_ zur Generalisierung definiert

--- morphclock.js
+++ morphclock.js
@@ -1,14 +1,18 @@
+var img_slot = {};
+
+var time_fomat;
+
 var imgData = { 
     'hhmm12':   { 'digit-hhmm12-Hx':'svg/md-00-00.svg',
                   'digit-hhmm12-xH':'svg/md-00-00.svg',
-                  'colon-hhmm12-CC':'svg/md-::-00.svg',
+                  'colon-hhmm12-Cx':'svg/md-::-00.svg',
                   'digit-hhmm12-Mx':'svg/md-00-00.svg',
                   'digit-hhmm12-xM':'svg/md-00-00.svg',
                   'alpha-hhmm12-Dx':'svg/md-ap-00.svg',
                   'alpha-hhmm12-xD':'svg/md-mm-00.svg' },
     'hhmm24':   { 'digit-hhmm24-Hx':'svg/md-00-00.svg',
                   'digit-hhmm24-xH':'svg/md-00-00.svg',
-                  'colon-hhmm24-CC':'svg/md-::-00.svg',
+                  'colon-hhmm24-Cx':'svg/md-::-00.svg',
                   'digit-hhmm24-Mx':'svg/md-00-00.svg',
                   'digit-hhmm24-xM':'svg/md-00-00.svg' },
     'hhmmss12': { 'digit-hhmmss12-Hx':'svg/md-00-00.svg',
@@ -30,18 +34,27 @@ var imgData = {
                   'digit-hhmmss24-Sx':'svg/md-00-00.svg',
                   'digit-hhmmss24-xS':'svg/md-00-00.svg' }
 };
-  
-function setImageSlots() {
+
+function getMorphclockElement(){
+    return document.getElementById('morphclock');
+}
+
+function getTimeFormat() {
     // get time format
-    var div = document.getElementById('morphclock');
-    var format = div.getAttribute('data-format');
-    console.log(format);
+    var format = getMorphclockElement().getAttribute('data-format');
+    return format;
+}
+
+function setImageSlots(format) {
+    var image_slots = {};
     // initialize images
     for (var id in imgData[format]) {
         var img = document.createElement('img');
         img.setAttribute('src', imgData[format][id]);
         img.setAttribute('id', id);
-        div.appendChild(img);
+        getMorphclockElement().appendChild(img);
+        image_slots[id] = img;
     }
+    return image_slots;
 }
 
