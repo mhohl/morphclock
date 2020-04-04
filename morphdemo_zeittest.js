5b2e5965c9171f4a3d4ffc73c7352c541aaa6662
Marc Hohl <marc@hohlart.de>
2020-04-04 19:22:18 +0200

    Schaltsekunden simulieren

--- morphdemo_zeittest.js
+++ morphdemo_zeittest.js
@@ -1,12 +1,17 @@
 /* morphdemo_timetest.js */
 var offset = 0;
-$('input').change(function() {
+var leap = 0;
+$('input[type=number]').change(function() {
     offset = $("#offset_year").val() * 365 * 24 * 60 * 60 * 1000
            + $("#offset_month").val() * 30 * 24 * 60 * 60 * 1000
                   + $("#offset_day").val() * 24 * 60 * 60 * 1000
                       + $("#offset_hour").val() * 60 * 60 * 1000
                          + $("#offset_minute").val() * 60 * 1000
                               + $("#offset_second").val() * 1000;
+});
+$("input[name=leap]").change(function() {
+    leap = $(this).val();
+    Morph.io.leap = leap;
 });
 MorphDisplay.prototype.update = function() {
     let now = new MorphTimeDate(offset);
